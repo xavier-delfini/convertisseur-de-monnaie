668c181b6956ae14dd6610eac6189d053b85bda7
xavier-delfini <xavier.delfini@laplateforme.io>
2023-01-20 02:03:51 +0100

    Fonction insert devise fini

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 from tkinter import *
 from tkinter import ttk
 import json
-change=1.08
-def convert(amount):
-    return amount * change
-print(convert(1000.94))
 
-""""
-myfamily = {
-  "dollar" : {
-    "name" : "Dollar américain",
-    "year" : 2004
-  },
-  "Ye" : {
-    "name" : "Tobias",
-    "year" : 2007
-  },
-  "child3" : {
-    "name" : "Linus",
-    "year" : 2011
-  }
-}
-json_value = json.dumps(myfamily,indent=3)
+change = 1.08
 
-f = open("test.json", "a")
-f.write(json_value)
-f.close()
+def convert(amount,devise_1,devise_2):
+
+        if devise_1[0] == "euro" or devise_2[0] == "euro":
+            return amount * change
+print(convert(1000,["euro",1],["dolard",1.08]))
+
+def fetch_array():
+    f = open("test.json", "r")
+    array = f.read()
+    f.close
+    return json.loads(array)
+def insert(name,value):
+    current_list=fetch_array()
+    for x in current_list:
+        if x[0]== name:
+            print("Cette devise existe déjà")
+            return 0
+    insert_array = [[name, value]]
+    current_list.extend(insert_array)
+    f = open("test.json", "w")
+    json_value=json.dumps(current_list)
+    f.write(json_value)
+    f.close()
+
+insert("test",13)
+"""
+f = open("test.json", "r")
+dictio=f.read()
+y = json.loads(dictio)
 """
